6b7bb53509bcff6679cb0910b99c21093ce42c0c
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-02-08 17:12:22 +0800

    build: top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [react()],
   resolve: {
     alias: {
