a6a754547ccf3220cb4b4390d439b08a4cc505da
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-02-13 10:49:54 +0800

    feat: reset layout support

--- src/app/Playground/index.tsx
+++ src/app/Playground/index.tsx
@@ -1,26 +1,36 @@
-import type { FC } from 'react';
+import { type FC, useRef } from 'react';
+import type { ImperativePanelGroupHandle } from 'react-resizable-panels';
+
 import Editor from './Editor';
 import FileTabs from './FileTabs';
 import Container from '~/components/Container';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '~/components/ui/Resizable';
 
-const Playground: FC = () => (
-  <ResizablePanelGroup direction="horizontal">
-    <ResizablePanel>
-      <Container title="Editor">
-        <FileTabs />
-        <Editor />
-      </Container>
-    </ResizablePanel>
+const Playground: FC = () => {
+  const resizableRef = useRef<ImperativePanelGroupHandle>(null);
 
-    <ResizableHandle />
+  const handleResetLayout = () => {
+    resizableRef.current && resizableRef.current.setLayout([50, 50]);
+  };
 
-    <ResizablePanel>
-      <Container title="Editor">
-        <div className="h-full">123</div>
-      </Container>
-    </ResizablePanel>
-  </ResizablePanelGroup>
-);
+  return (
+    <ResizablePanelGroup direction="horizontal" ref={resizableRef}>
+      <ResizablePanel>
+        <Container title="Editor">
+          <FileTabs />
+          <Editor />
+        </Container>
+      </ResizablePanel>
+
+      <ResizableHandle onDoubleClick={handleResetLayout} />
+
+      <ResizablePanel>
+        <Container title="Editor">
+          <div className="h-full">123</div>
+        </Container>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
 
 export default Playground;
