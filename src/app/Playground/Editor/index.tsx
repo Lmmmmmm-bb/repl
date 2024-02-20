9fdeaad4d9b2b8fdfcc44e0a2b02a62e1e4d197c
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-02-20 19:06:19 +0800

    chore(hooks): useEditor

--- src/app/Playground/Editor/index.tsx
+++ src/app/Playground/Editor/index.tsx
@@ -1,46 +1,8 @@
 import type { FC } from 'react';
-import { useEffect, useRef } from 'react';
-import type { editor } from 'monaco-editor';
-import { getOrCreateModel, monaco, monacoOptions } from '~/monaco';
-import { updateFileContent, useVirtualFileStore } from '~/stores/virtual-file';
+import { useEditor } from '~/hooks/useEditor';
 
 const Editor: FC = () => {
-  const { activeFile } = useVirtualFileStore();
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  const editorRef = useRef<editor.IStandaloneCodeEditor>();
-
-  useEffect(() => {
-    if (!containerRef.current) {
-      throw new Error('Cannot find editor container');
-    }
-
-    const monacoInstance = monaco.editor.create(containerRef.current, monacoOptions);
-    monacoInstance.onDidChangeModelContent(() => {
-      updateFileContent(monacoInstance.getValue());
-    });
-    monacoInstance.addCommand(
-      monaco.KeyMod.Shift | monaco.KeyMod.Alt | monaco.KeyCode.KeyF,
-      () => monacoInstance.trigger('format-code', 'format-code', null),
-    );
-
-    editorRef.current = monacoInstance;
-
-    return () => {
-      editorRef.current && editorRef.current.dispose();
-      editorRef.current = undefined;
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!editorRef.current || !activeFile) {
-      return;
-    }
-
-    const model = getOrCreateModel(activeFile);
-    editorRef.current.setModel(model);
-    editorRef.current.focus();
-  }, [activeFile]);
+  const { containerRef } = useEditor();
 
   return (
     <div
---
+++ src/hooks/useEditor.ts
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+import type { editor } from 'monaco-editor';
+import { getOrCreateModel, monaco, monacoOptions } from '~/monaco';
+import { updateFileContent, useVirtualFileStore } from '~/stores/virtual-file';
+
+export const useEditor = () => {
+  const { activeFile } = useVirtualFileStore();
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const editorRef = useRef<editor.IStandaloneCodeEditor>();
+
+  useEffect(() => {
+    if (!containerRef.current) {
+      throw new Error('Cannot find editor container');
+    }
+
+    const monacoInstance = monaco.editor.create(containerRef.current, monacoOptions);
+    monacoInstance.onDidChangeModelContent(() => {
+      updateFileContent(monacoInstance.getValue());
+    });
+
+    editorRef.current = monacoInstance;
+
+    return () => {
+      editorRef.current && editorRef.current.dispose();
+      editorRef.current = undefined;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!editorRef.current || !activeFile) {
+      return;
+    }
+
+    const model = getOrCreateModel(activeFile);
+    editorRef.current.setModel(model);
+    editorRef.current.focus();
+  }, [activeFile]);
+
+  return { containerRef };
+};
