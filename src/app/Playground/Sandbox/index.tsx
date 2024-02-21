f98c89ab03dbe4da75f099a1a420fdb7ee455167
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-02-21 14:15:20 +0800

    chore(playground): useCompilerWorker

--- src/app/Playground/Sandbox/index.tsx
+++ src/app/Playground/Sandbox/index.tsx
@@ -1,8 +1,8 @@
 import { type FC, useEffect, useRef } from 'react';
 import { sandboxAttr } from './config';
-import CompilerWorker from './compiler.worker?worker&inline';
 import playSrcdoc from './play.html?raw';
 import { getPlaySrcdoc } from './utils';
+import { useCompilerWorker } from './useCompilerWorker';
 import { useThemeStore } from '~/stores/theme';
 import { useVirtualFileStore } from '~/stores/virtual-file';
 import { usePackageStore } from '~/stores/package';
@@ -13,33 +13,20 @@ const Sandbox: FC = () => {
   const theme = useThemeStore(state => state.theme);
 
   const sandboxRef = useRef<HTMLIFrameElement>(null);
-  const compilerWorkerRef = useRef<Worker | null>(null);
-
   const sendSandboxMessage = (message: any) => {
     sandboxRef.current
     && sandboxRef.current.contentWindow
     && sandboxRef.current.contentWindow.postMessage(message, location.origin);
   };
 
-  const sendCompilerWorkerMessage = (message: any) => {
-    compilerWorkerRef.current && compilerWorkerRef.current.postMessage(message);
-  };
+  const { sendWorkerMessage } = useCompilerWorker((event: MessageEvent) => {
+    const payload = event.data;
+    sendSandboxMessage(payload);
+  });
 
   const handleSandboxLoad = () => {
-    sendCompilerWorkerMessage({ files });
+    sendWorkerMessage({ files });
   };
-
-  useEffect(() => {
-    if (compilerWorkerRef.current) {
-      return;
-    }
-
-    compilerWorkerRef.current = new CompilerWorker();
-    compilerWorkerRef.current.addEventListener('message', (event: MessageEvent) => {
-      const payload = event.data;
-      sendSandboxMessage(payload);
-    });
-  }, []);
 
   useEffect(() => {
     if (sandboxRef.current) {
@@ -48,8 +35,8 @@ const Sandbox: FC = () => {
   }, [extraPackages]);
 
   useEffect(() => {
-    sendCompilerWorkerMessage({ files });
-  }, [files]);
+    sendWorkerMessage({ files });
+  }, [files, sendWorkerMessage]);
 
   useEffect(() => {
     sendSandboxMessage({ type: 'THEME_CHANGE', data: theme });
