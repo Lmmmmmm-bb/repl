5e54f6f0f9770d5361579182beb03e9d0abe3fa4
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-02-25 17:47:29 +0800

    style: add tab tips

--- src/app/Playground/FileTabs/NewFile.tsx
+++ src/app/Playground/FileTabs/NewFile.tsx
@@ -1,16 +1,26 @@
 import { useRef, useState } from 'react';
 import type { FC, KeyboardEvent } from 'react';
 import { virtualFileExtIconStrategy } from './strategy';
+import { useErrorMessage } from './useErrorMessage';
 import Tabs from '~/components/Tabs';
 import Plus from '~/icons/Plus';
 import File from '~/icons/File';
 import { getVirtualFileExt, isValidFilename } from '~/virtual-file';
 import { useToggle } from '~/hooks/useToggle';
 import { addFile } from '~/stores/virtual-file';
+import { Popover, PopoverContent, PopoverTrigger } from '~/components/ui/Popover';
+import { cn } from '~/utils/cn';
 
 const NewFile: FC = () => {
   const addTabItemRef = useRef<HTMLLIElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const {
+    open,
+    errorMessage,
+    updateMessage,
+    clearMessage,
+  } = useErrorMessage();
 
   const [pending, togglePending] = useToggle();
   const [inputValue, setInputValue] = useState('');
@@ -27,6 +37,7 @@ const NewFile: FC = () => {
   };
 
   const resetNewFileState = () => {
+    clearMessage();
     setInputValue('');
     togglePending.off();
   };
@@ -36,12 +47,24 @@ const NewFile: FC = () => {
       resetNewFileState();
       return;
     }
-    addFile(inputValue);
-    setInputValue('');
-    togglePending.off();
-    setTimeout(() => {
-      addTabItemRef.current && addTabItemRef.current.scrollIntoView();
-    });
+
+    try {
+      addFile(inputValue);
+      setInputValue('');
+      togglePending.off();
+      setTimeout(() =>
+        addTabItemRef.current && addTabItemRef.current.scrollIntoView(),
+      );
+      clearMessage();
+    } catch (error) {
+      if (error instanceof Error) {
+        const isFileExistError = error.message.includes('already exists');
+        updateMessage({
+          type: isFileExistError ? 'warning' : 'error',
+          message: error.message,
+        });
+      }
+    }
   };
 
   const handleInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
@@ -52,27 +75,41 @@ const NewFile: FC = () => {
   };
 
   return (
-    <Tabs.Item title="New file" ref={addTabItemRef} onClick={handleAddFile}>
-      {pending
-        ? (
-          <>
-            {isValidFilename(inputValue)
-              ? virtualFileExtIconStrategy[getVirtualFileExt(inputValue)]
-              : <File className="w-4 h-4 opacity-60" />}
-            <input
-              spellCheck={false}
-              className="w-24 outline-none bg-transparent"
-              placeholder="input filename..."
-              ref={inputRef}
-              value={inputValue}
-              onBlur={handleAddFileDone}
-              onKeyDown={handleInputKeyDown}
-              onChange={e => setInputValue(e.target.value)}
-            />
-          </>
-          )
-        : <Plus className="w-4 h-4" />}
-    </Tabs.Item>
+    <Popover open={open}>
+      <PopoverTrigger asChild>
+        <Tabs.Item title="New file" ref={addTabItemRef} onClick={handleAddFile}>
+          {pending
+            ? (
+              <>
+                {isValidFilename(inputValue)
+                  ? virtualFileExtIconStrategy[getVirtualFileExt(inputValue)]
+                  : <File className="w-4 h-4 opacity-60" />}
+                <input
+                  spellCheck={false}
+                  placeholder="input filename..."
+                  className="w-24 outline-none bg-transparent"
+                  ref={inputRef}
+                  value={inputValue}
+                  onBlur={handleAddFileDone}
+                  onKeyDown={handleInputKeyDown}
+                  onChange={e => setInputValue(e.target.value)}
+                />
+              </>
+              )
+            : <Plus className="w-4 h-4" />}
+        </Tabs.Item>
+      </PopoverTrigger>
+
+      <PopoverContent
+        className={cn(
+          ['p-2 text-white text-xs font-mono'],
+          errorMessage.type === 'error' && ['bg-destructive'],
+          errorMessage.type === 'warning' && ['bg-orange-500', 'dark:bg-orange-800'],
+        )}
+      >
+        {errorMessage.message}
+      </PopoverContent>
+    </Popover>
   );
 };
 
