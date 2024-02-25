b1cce332fb1078044d5daf9e5a4fa03dd115fbe3
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-02-25 21:56:09 +0800

    chore: specify package version before register

--- src/monaco/core-lib.ts
+++ src/monaco/core-lib.ts
@@ -2,7 +2,19 @@ import { registerExtraLib } from './extra-lib';
 import { fetchPackageFiles } from '~/apis/package-metadata';
 import { fetchPackageFileRaw } from '~/apis/package-raw';
 import { fetchPackageVersionList } from '~/apis/package-version-list';
-import { type CorePackage, initPackageStore } from '~/stores/package';
+import { type CorePackage, initPackageStore, usePackageStore } from '~/stores/package';
+
+const beforeRegisterCorePackage = async () => {
+  const corePackagesWithVersion: CorePackage[] = await Promise.all(
+    initPackageStore.corePackages.map(async (lib) => {
+      const versions = await fetchPackageVersionList(lib.name);
+      const latestVersion = versions.tags.latest;
+      return { ...lib, version: latestVersion };
+    }),
+  );
+
+  usePackageStore.setState({ corePackages: corePackagesWithVersion });
+};
 
 const registerCorePackage = async (lib: CorePackage) => {
   const packageName = lib.name;
@@ -26,7 +38,9 @@ const registerCorePackage = async (lib: CorePackage) => {
   return corePackage;
 };
 
-export const initCoreLib = () => {
+export const initCoreLib = async () => {
+  await beforeRegisterCorePackage();
+
   const EXCLUDE_PACKAGE_NAME = ['react', 'react-dom'];
   initPackageStore.corePackages
     .filter(item => !EXCLUDE_PACKAGE_NAME.includes(item.name))
