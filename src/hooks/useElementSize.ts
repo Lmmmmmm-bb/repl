628bff3c1aebc8279b298919fd72bfec8c74c870
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-08-22 14:22:44 +0800

    chore: deps upgrade

--- src/hooks/useElementSize.ts
+++ src/hooks/useElementSize.ts
@@ -1,6 +1,6 @@
 import { type RefObject, useEffect, useRef, useState } from 'react';
 
-export const useElementSize = (elRef: RefObject<HTMLElement | null>) => {
+export const useElementSize = (elRef: RefObject<HTMLDivElement | null>) => {
   const [elementSize, setElementSize] = useState({ width: 0, height: 0 });
   const observerRef = useRef<ResizeObserver | null>(null);
 
